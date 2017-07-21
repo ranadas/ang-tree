1ebe445983b564212f46e8636dd70b4d29c292d2
rdas <rana.das@hrblock.com>
2017-07-21 20:36:06 +0100

    wip - parent tree

---
+++ tree-parent.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//https://stackoverflow.com/questions/35198970/how-to-check-all-children-based-on-parent-while-also-checking-parent-based-on-ch
+//https://plnkr.co/edit/sQfA2UL5dRC601R1QQul?p=preview
+
+//https://plnkr.co/edit/Tt96EE2rruy1HAudRVJR?p=preview
+var angMod = angular.module('treeParent', ['']);
+HomeController.$inject = ['$timeout'];
+function HomeController($timeout) {
+    var self = this;    
+}
+
+angMod.controller('treeController', HomeController);
